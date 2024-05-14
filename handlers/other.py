b2dbcf3ae605086922ccc34a3bdd8eae4ba82628
Almaz Galimov <almazwork@gmail.com>
2024-05-14 16:55:38 +0500

    add l18n by gettext

--- handlers/other.py
+++ handlers/other.py
@@ -1,5 +1,6 @@
 from aiogram import Router
 from aiogram.types import Message
+from aiogram.utils.i18n import gettext as _
 
 
 other_router = Router()
@@ -11,6 +12,6 @@ async def send_echo(message: Message):
         await message.send_copy(chat_id=message.chat.id)
     except TypeError as e:
         await message.reply(
-            text=f'Данный тип апдейтов не поддерживается методом send_copy'
+            text=_('Данный тип апдейтов не поддерживается методом send_copy')
         )
         print(e)
--- handlers/user.py
+++ handlers/user.py
@@ -6,6 +6,7 @@ from aiogram.types import (
     InlineKeyboardMarkup,
     Message
 )
+from aiogram.utils.i18n import gettext as _
 
 user_router = Router()
 
@@ -14,16 +15,16 @@ user_router = Router()
 async def start(message: Message):
     username = html.quote(message.from_user.full_name)
     button = InlineKeyboardButton(
-        text='Кнопка',
+        text=_('Кнопка'),
         callback_data='button_pressed'
     )
     markup = InlineKeyboardMarkup(inline_keyboard=[[button]])
     await message.answer(
-        text='Привет, {username}! Нажми на кнопку'.format(username=username),
+        text=_('Привет, {username}! Нажми на кнопку').format(username=username),
         reply_markup=markup
     )
 
 
 @user_router.callback_query(F.data == 'button_pressed')
 async def button_pressed(call: CallbackQuery):
-    await call.answer(text='Вы нажали на кнопку')
+    await call.answer(text=_('Вы нажали на кнопку'))
